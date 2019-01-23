eef1389cf6a83e807998fcc9416623d53868e2cf
Henry Moland <henrymoland@gmail.com>
2019-01-22 21:19:25 -0800

    Created Express server, added endpoints for register, login and users

---
+++ data/migrations/20190122190417_users-table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', user => {
+        user.increments();
+        user.string('username', 128)
+        .notNullable()
+        .unique();
+        user.string('password', 128)
+        .notNullable();
+        user.string('department', 128).notNullable();
+    })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
